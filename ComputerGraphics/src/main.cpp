e6fec83bb762d4ae54b8b3b8ee3511480f1b909e
Ali Efe GÜR <alife1029@gmail.com>
2023-09-15 13:18:38 +0300

    GLFW and Window Creation

--- ComputerGraphics/src/main.cpp
+++ ComputerGraphics/src/main.cpp
@@ -1,7 +1,32 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World" << std::endl;
+	if (!glfwInit())
+	{
+		std::cout << "Failed to initialize GLFW!" << std::endl;
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(1024, 680, "Computer Graphics Playground", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Falied to create GLFW window!" << std::endl;
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
